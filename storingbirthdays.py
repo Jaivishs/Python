704712f59fa7e96f6b16a2aa039cfa2ab400ab7c
Jaivishs <jaivishshah@gmail.com>
2025-12-20 13:34:47 +0000

    Add files via upload

---
+++ storingbirthdays.py
@@ -0,0 +1,14 @@
+#ask for birthdays
+bday1 = input("Enter birthday of friend 1: ")
+bday2 = input("Enter birthday of friend 2: ")
+bday3 = input("Enter birthday of friend 3: ")
+bday4 = input("Enter birthday of friend 4: ")
+bday5 = input("Enter birthday of friend 5: ")
+#printing the birthdays 
+print("\nStored Birthdays: ")
+print("The birthday of friend 1 is ", bday1)
+print("The birthday of friend 2 is ", bday2)
+print("The birthday of friend 3 is ", bday3)
+print("The birthday of friend 4 is ", bday4)
+print("The birthday of friend 5 is ", bday5)
+
