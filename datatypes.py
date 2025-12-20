53514e504c760b9238406a421b1820ee966b80b8
Jaivishs <jaivishshah@gmail.com>
2025-12-20 10:39:40 +0000

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a= 6
+print("The type of a is: ", type(a))
+b= 3.14159
+print("The type of b is: ", type(b))
+c= "Codingal" 
+print("The type of c is: ", type(c))
+d= True
+print("The type of d is: ", type(d))
