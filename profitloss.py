df67f4747089d7f17c41fa842adbd868044ff386
Jaivishs <jaivishshah@gmail.com>
2025-12-27 10:26:03 +0000

    Update profitloss.py

--- profitloss.py
+++ profitloss.py
@@ -1,8 +1,9 @@
-actualcost = float(input("Enter the Actual Porduct Price: "))
+actualcost = float(input("Enter the Actual Product Price: "))
 saleamount = float(input("Enter the sales amount: "))
 
 if (saleamount > actualcost):
     amount = saleamount - actualcost
     print("Total profit = {0}".format(amount))
 else:
+
     print("No profit!!")
