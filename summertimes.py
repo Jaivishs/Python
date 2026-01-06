543c395f967d2c2398c06855e8160b93aeea1309
Jaivishs <jaivishshah@gmail.com>
2026-01-06 17:59:06 +0000

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,8 @@
+temp = int(input("Enter the current temperature in Celcius:  "))
+
+if temp >= 25:
+    print("It's warm enough, wear light clothes")
+elif temp >=15:
+    print("It's a bit cold, wear something a bit warmer")
+else:
+    print("It's cold outside, you should wear a jacket and pullover.")
