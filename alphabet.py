f1f1c5fe1ae28f0fc237e357c2ab17dfc8f641ca
Jaivishs <jaivishshah@gmail.com>
2026-01-13 18:57:03 +0000

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character = ")
+if (ch in "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"):
+    print("The character is in the alphabet")
+else:
+    print("The character is not in the alphabet")
+    
