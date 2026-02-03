80da43796ff05c708b9bf7ab95f2164a848c0da8
Jaivishs <jaivishshah@gmail.com>
2026-02-03 18:07:22 +0000

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,9 @@
+age = int(input("Please enter your age: "))
+
+if age<=20:
+    if age>=10:
+        print("You are allowed in the class")
+    else:
+        print("You are not allowed in the class")
+else:
+    print("You are not allowed in the class")
